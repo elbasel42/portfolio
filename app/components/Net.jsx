8c8b6856f389f24c3294385a436ee3001c4363a8
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-03-04 18:12:47 +0200

    Minor change

--- app/components/Net.jsx
+++ app/components/Net.jsx
@@ -66,19 +66,11 @@ export const Net = () => {
       ></div>
       <Script
         src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js"
-        // onLoad={() => setThreeLoaded(true)}
-        onReady={() => {
-          console.log("three loaded");
-          setThreeReady(true);
-        }}
+        onReady={() => setThreeReady(true)}
       />
       <Script
         src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.net.min.js"
-        // onLoad={() => setVantaLoaded(true)}
-        onReady={() => {
-          console.log("Vanta loaded");
-          setVantaReady(true);
-        }}
+        onReady={() => setVantaReady(true)}
       />
     </>
   );
