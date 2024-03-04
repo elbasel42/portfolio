822e38430bcfab421f5f5fc13396e4e51816f39c
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-03-04 21:28:10 +0200

    Add back arrow

--- app/components/AboutPage/AboutPageTitle.tsx
+++ app/components/AboutPage/AboutPageTitle.tsx
@@ -1,6 +1,6 @@
 import { Pacifico } from "next/font/google";
 import { twMerge } from "tailwind-merge";
-import { SlideIn, HackedText } from "@app/components";
+import { SlideIn } from "@app/components";
 
 const pacifico = Pacifico({
   subsets: ["latin"],
@@ -13,7 +13,10 @@ interface AboutPageProps {
 export const AboutPageTitle = ({ text }: AboutPageProps) => {
   return (
     <SlideIn
-      className={twMerge("mt-20 text-red-600 text-6xl text-center", pacifico.className)}
+      className={twMerge(
+        "mt-8 md:mt-20 text-red-600 text-6xl text-center",
+        pacifico.className
+      )}
     >
       {text}
     </SlideIn>
--- app/components/AboutPage/Bio.tsx
+++ app/components/AboutPage/Bio.tsx
@@ -1,18 +1,18 @@
 export const Bio = () => {
   return (
-    <div className="w-[70vw] bg-black/80 rounded-3xl h-[50vh] app-scrollbar overflow-y-auto py-2 px-4 mt-8 font-mono text-xl text-center text-blue-600">
-      <p className="my-4">
+    <div className="w-full md:w-[70vw] border-white/50 border overflow-x-hidden bg-black/80 rounded-3xl text-lg h-[60vh] app-scrollbar overflow-y-auto py-2 px-4 mt-8 font-mono md:text-xl text-center text-blue-600">
+      <p className="my-8">
         Hello! My name is Abdelrahman, I&apos;m a Front-developer from Egypt. I
         started out my career in tech as a technical support specialist,
         assisting customers with technical queries among other things.
       </p>
-      <p className="my-4">
+      <p className="my-8">
         I then got interested in switching careers to software development after
         which I started out as Data Analyst for a call center company where I
         managed call ques and generated reports for clients using Python, numpy
         and pandas.
       </p>
-      <p className="my-4">
+      <p className="my-8">
         After a year of working as a Data Analyst I got curious about other
         opportunities in tech that I can work in, which put me on the way to Web
         Development, since part of my job as a data analyst was generating
@@ -20,7 +20,7 @@ export const Bio = () => {
         presentation processes using a popular dashboard library called{" "}
         <i>streamlit</i>
       </p>
-      <p className="my-4">
+      <p className="my-8">
         I quickly fell in love with Python and the power it gave me, I ended up
         automating 80% of my job! I then used TheOdinProject online curriculum
         and several youtube channels to start my journey in Web Development.
