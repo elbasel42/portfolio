30f1c502f25529fbc0508008fb9cc93e0da8110d
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-03-06 00:12:58 +0200

    Add vercel analytics

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -1,5 +1,6 @@
 import { type Metadata } from "next";
 import { type ReactNode } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "../globals.css";
 
@@ -18,6 +19,7 @@ const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 };
