adcc84fdaedea79eae901c346747c9c0693b43b1
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-03-06 19:22:59 +0200

    Add homebutton to projects page

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,3 +1,4 @@
+import { HomeButton } from "@app/components";
 import { Pacifico } from "next/font/google";
 import { twMerge } from "tailwind-merge";
 
@@ -8,6 +9,7 @@ const pacifico = Pacifico({
 const ProjectsPage = () => {
   return (
     <main className="h-[100svh] flex items-center justify-center">
+      <HomeButton />
       <h1
         className={twMerge(
           "md:text-9xl text-4xl text-purple-600 md:underline decoration-orange-600 decoration-wavy text-center",
