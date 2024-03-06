ba2d754d1558d18ca02b90f34ad8ab8ae2c20a12
Abdelrahman Elbasel <abdelrahman.elbasel42@gmail.com>
2024-03-06 15:38:40 +0200

    Adjust BackLink.tsx styles

--- app/components/BackLink.tsx
+++ app/components/BackLink.tsx
@@ -5,7 +5,7 @@ export const BackLink = () => {
   return (
     <Link
       href="/"
-      className="fixed z-20 px-2 py-2 transform border rounded-lg transition-all duration-2000 hover:rotate-[360deg] active:rotate-[360deg] top-4 border-white/50 right-4"
+      className="bg-black/80 fixed z-20 px-2 py-2 transform border rounded-lg transition-all duration-2000 hover:rotate-[360deg] active:rotate-[360deg] top-4 border-white/50 right-4"
     >
       <TbArrowBackUpDouble className="md:w-8 md:h-8 active:scale-125 hover:scale-125" />
     </Link>
