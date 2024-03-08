e8cfc5d3d27c22412249740bd261b7943b1f225e
El-basel <150071951+elbasel42@users.noreply.github.com>
2024-03-08 19:56:07 +0200

    Update Technologies.tsx

--- app/components/AboutPage/Technologies.tsx
+++ app/components/AboutPage/Technologies.tsx
@@ -6,26 +6,26 @@ export const Technologies = () => {
 
   return (
     <div className="flex mt-20 px-1 md:mt-8">
-      <div className="space-y-4">
+      <div className="space-y-4 ml-auto">
         {technologies.slice(0, separator).map((tech, index) => (
           <TechItem
             Logo={tech.icon}
             title={tech.title}
-            slideFrom="left"
+            slideFrom="right"
             animDuration={(index + 2) * 1000}
             className={tech.className}
             key={tech.title}
           />
         ))}
       </div>
-      <div className="space-y-4 overflow-hidden ml-auto">
+      <div className="space-y-4 overflow-hidden mr-auto">
         {technologies
           .slice(separator, technologies.length)
           .map((tech, index) => (
             <TechItem
               Logo={tech.icon}
               title={tech.title}
-              slideFrom="right"
+              slideFrom="left"
               animDuration={(index + 2) * 1000}
               className={tech.className}
               key={tech.title}
